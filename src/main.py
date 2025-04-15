4f75d3f22bb8f2d18a8f050509877a1306824b2d
shinshARK <137866950+shinshARK@users.noreply.github.com>
2025-04-15 17:28:49 +0700

    fix: remove text to html node fn from main

--- src/main.py
+++ src/main.py
@@ -3,23 +3,6 @@ from src.htmlnode import HTMLNode
 from src.leafnode import LeafNode
 from src.parentnode import ParentNode
 
-def text_node_to_html_node(text_node):
-    match(text_node.text_type):
-        case TextType.TEXT:
-            return LeafNode(None, text_node.text)
-        case TextType.BOLD:
-            return LeafNode("b", text_node.text)
-        case TextType.ITALIC:
-            return LeafNode("i", text_node.text)
-        case TextType.CODE:
-            return LeafNode("code", text_node.text)
-        case TextType.LINK:
-            return LeafNode("a", text_node.text, {"href": text_node.url})
-        case TextType.LINK:
-            return LeafNode("a", '', {"src": text_node.url, "alt": text_node.text})
-        case _:
-            raise Exception("invalid text type")
-            
 def main():
     # test = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     # print(test)
